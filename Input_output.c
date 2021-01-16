883121feb849b7c3a09692ed800096fe8bac4225
saurabh sharma <saurabhsharma29520@gmail.com>
2021-01-16 16:53:46 +0530

    Day 1 Commit

---
+++ Input_output.c
@@ -0,0 +1,18 @@
+// To program to learn the basic I/O operation....
+
+
+#include<stdio.h>
+
+int main(){
+   int i;
+   char ch;
+   printf("Enter your input :  \n");
+   scanf("%c",&ch);
+   printf("Enter your input :  \n");
+   scanf("%d",&i);
+   printf("Your input is : %d",i);
+
+   printf("Your input is : %c\n",ch);
+   printf("Hello World!...");
+   return 0;
+}
---
+++ Use_of_Data_type.c
@@ -0,0 +1,15 @@
+// To program use and implementation of data type in c...
+
+
+#include<stdio.h>
+
+int main()
+{
+   int page = 200;
+   float price = 215.25;
+   char ch = 'E';
+   printf("\n The page of the book :  %d",page);
+   printf("\n The price of the book : %f",price);
+   printf("\n The language of the book : %c",ch);
+   return 0;
+}
